0eb503419d9c42f10fb177bcfc24981c5bb659cf
Henderson-dev <teconet.arte@gmail.com>
2021-01-26 15:14:39 -0300

    Create design and css for footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -31,4 +31,71 @@ $(document).on("scroll", function () {
   }
 });
 
+
+// var swiper = new Swiper('.swiper-container', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },  
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  breakpoints: {
+    440: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },    
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },    
+    1199: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+  }  
+});
+
+// var mySwiper = new Swiper('.swiper-container', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+
+
 })(jQuery);
